1fd3efefa6f41230cc2c679b4ed0c6a41cd12616
Susan-Wolfgram <skwolf@uw.edu>
2015-10-18 17:11:14 -0700

    first commit

--- js/map.js
+++ js/map.js
@@ -1,35 +1,77 @@
-// Function to draw your map
+// Function to draw map
 var drawMap = function() {
-
-  // Create map and set view
- 
-
-  // Create a tile layer variable using the appropriate url
-
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
+ 	var map = L.map('container').setView([32.07, -94.79], 4);
+  	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+ 	layer.addTo(map)
+ 	getData(map) 
 }
 
 // Function for getting data
-var getData = function() {
-
-  // Execute an AJAX request to get the data in data/response.js
-
-
-  // When your request is successful, call your customBuild function
-
+var getData = function(map) {
+	$.getJSON('data/response.json', function(data) {
+		customBuild(data, map); 
+	}); 
 }
 
 // Loop through your data and add the appropriate layers and points
-var customBuild = function() {
-	// Be sure to add each layer to the map
+var customBuild = function(data, map) {
+	var layerMale = new L.LayerGroup([]);
+	var layerFemale = new L.LayerGroup([]); 
+	var underTwenty = new L.LayerGroup([]); 
+	var twenties = new L.LayerGroup([]); 
+	var thirties = new L.LayerGroup([]); 
+	var forties = new L.LayerGroup([]);
+	var fifties = new L.LayerGroup([]);
+	var overSixty =  new L.LayerGroup([]);
+	var killed = 0; 
+	var male = 0; 
+	var female = 0; 
+	var killedMale = 0; 
+	for (var i=0; i<data.length;i++) {
+			var lat = data[i]["lat"];
+			var lng = data[i]["lng"];
+			var circ = L.circle([lat, lng], 750);
+			circ.bindPopup(data[i]["Summary"]);
+			var age = parseInt(data[i]["Victim's Age"]);
+			if (data[i]["Hit or Killed?"] == "Killed") {
+				circ.setStyle({color: 'red', fillColor: 'red'});
+				killed++
+				if (data[i]["Victim's Gender"] == "Male") {
+					killedMale++
+				}
+			}
+			if (age < 20) {
+				circ.addTo(underTwenty);
+			} else if (age >= 20 && age <= 29) {
+				circ.addTo(twenties);
+			} else if (age >= 30 && age <= 39) {
+				circ.addTo(thirties);
+			} else if (age >= 40 && age <= 49) {
+				circ.addTo(forties);
+			} else if (age >= 50 && age <= 59) {
+				circ.addTo(fifties);
+			} else if (age >= 60) {
+				circ.addTo(overSixty);
+			}
+			if (data[i]["Victim's Gender"] == "Male") {
+				circ.addTo(layerMale);
+				male++
+			} else {
+				circ.addTo(layerFemale);
+				female++
+			}	
+	}
 
-	// Once layers are on the map, add a leaflet controller that shows/hides layers
-  
+	var layers = {"Male Victims" : layerMale, "Female/Unspecified Victims" : layerFemale, 
+	"Victims Under 20" : underTwenty, "Victims Age 20-29" : twenties, 
+	"Victims Age 30-39" : thirties, "Victims Age 40-49" : forties,
+	"Victims Age 50-59" : fifties, "Victims Over 60" : overSixty};
+	L.control.layers(null, layers).addTo(map);
+
+	$('#killedMen').text(killedMale);
+	$('#killedWomen').text(killed - killedMale);
+	$('#injuredM').text(male - killedMale);
+	$('#injuredW').text(female - (killed - killedMale));
 }
 
 
